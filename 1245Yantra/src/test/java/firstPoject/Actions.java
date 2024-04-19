89f4d03830578a62f745321185b67af69efba58c
CSC <CSC@Roopak07>
2024-04-18 13:19:24 -1200

    bhai azgar

---
+++ 1245Yantra/src/test/java/firstPoject/Actions.java
@@ -0,0 +1,19 @@
+package firstPoject;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Actions {
+	public static void main(String[] args) {
+		WebDriver x=new ChromeDriver();
+		x.get("https://demoapps.qspiders.com/ui/button/buttonRight?sublist=1");
+		x.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
+		WebElement z = x.findElement(By.xpath("//button[@id='btn30']"));
+	
+	}
+
+}
